309962974c530f29910db1929f56e954d32daece
Bart Kastermans <kasterma@kasterma.net>
2023-12-24 08:49:11 +0100

    chore: mypy strict and remove unneeded **kwargs

--- src/simulation.py
+++ src/simulation.py
@@ -1,4 +1,5 @@
-from typing import TypeAlias, TypeVar
+from abc import ABC, abstractmethod
+from typing import TypeAlias, TypeVar, Tuple
 from collections.abc import Iterable, Callable
 from datetime import datetime, timedelta
 from random import Random
@@ -45,22 +46,29 @@ class LoginAttempt:
     status: int
     time: datetime
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"LoginAttempt({self.user}, {self.status}, {self.time.strftime('%H:%M:%S')})"
 
 
-class UserAction:
-    def __call__(self, *args, **kwargs):
+class UserAction(ABC):
+    @abstractmethod
+    def __call__(
+        self, time: datetime, user: int
+    ) -> Tuple[list[LoginAttempt], datetime]:
         ...
 
 
 class Login(UserAction):
-    def __call__(self, time: datetime, user: int, **kwargs):
+    def __call__(
+        self, time: datetime, user: int
+    ) -> Tuple[list[LoginAttempt], datetime]:
         return [LoginAttempt(user=user, status=200, time=time)], time
 
 
 class LoginFail(UserAction):
-    def __call__(self, time: datetime, user: int, **kwargs):
+    def __call__(
+        self, time: datetime, user: int
+    ) -> Tuple[list[LoginAttempt], datetime]:
         return [LoginAttempt(user=user, status=401, time=time)], time
 
 
@@ -68,8 +76,10 @@ class Pause(UserAction):
     def __init__(self, duration: timedelta | int):
         self.duration = ensuretimedelta(duration)
 
-    def __call__(self, time: datetime, **kwargs):
-        return None, time + self.duration
+    def __call__(
+        self, time: datetime, user: int
+    ) -> Tuple[list[LoginAttempt], datetime]:
+        return [], time + self.duration
 
 
 class PauseBetween(UserAction):
@@ -77,8 +87,10 @@ class PauseBetween(UserAction):
         self.lower = ensuretimedelta(lower)
         self.upper = ensuretimedelta(upper)
 
-    def __call__(self, time: datetime, **kwargs):
-        return None, time + timedelta(
+    def __call__(
+        self, time: datetime, user: int
+    ) -> Tuple[list[LoginAttempt], datetime]:
+        return [], time + timedelta(
             seconds=sim_random.randint(
                 round(self.lower.total_seconds()), round(self.upper.total_seconds())
             )
@@ -87,23 +99,22 @@ class PauseBetween(UserAction):
 
 class User:
     def __init__(
-        self,
-        behaviors: list[UserAction] | None = None,
-        **kwargs,
+        self, behaviors: list[UserAction] | None = None, user: int | None = None
     ):
         self._behaviors = behaviors or []
-        self._passed_kwargs = kwargs
-        self._kwargs = {"user": get_user_id(), "ignored": None} | kwargs
+        self._passed_user: int | None = user
+        self._user: int = user or get_user_id()
 
-    def __call__(self, start: datetime, **kwargs):
+    def __call__(
+        self, start: datetime, duration_seconds: int = 0
+    ) -> list[LoginAttempt]:
         rv = []
         for behavior in self._behaviors:
-            act, start = behavior(start, **(self._kwargs | kwargs))
-            if act:
-                rv.extend(act)
+            act, start = behavior(start, user=self._user)
+            rv.extend(act)
         return rv
 
-    def __add__(self, other):
+    def __add__(self, other: UserAction) -> "User":
         if isinstance(other, UserAction):
             self._behaviors.append(other)
         else:
@@ -112,27 +123,20 @@ class User:
 
 
 class UserGroup:
-    def __init__(self, user: User, count: int, **kwargs):
-        self._passed_kwargs = kwargs
+    def __init__(self, user: User, count: int):
         self._users = [
-            User(
-                user._behaviors,
-                **(user._passed_kwargs | kwargs),
-            )
-            for _ in range(count)
+            User(user._behaviors, user=user._passed_user) for _ in range(count)
         ]
 
-    def __add__(self, other):
-        rv = UserGroup(User(), 0, **self._passed_kwargs)
+    def __add__(self, other: "UserGroup") -> "UserGroup":
+        rv = UserGroup(User(), 0)
         rv._users.extend(self._users)
         rv._users.extend(other._users)
         return rv
 
-    def __call__(self, start: datetime, duration_seconds: int, **kwargs):
+    def __call__(self, start: datetime, duration_seconds: int) -> list[LoginAttempt]:
         starttimes = generate_times(start, duration_seconds, len(self._users))
-        return flatten(
-            user(start, **kwargs) for user, start in zip(self._users, starttimes)
-        )
+        return flatten(user(start) for user, start in zip(self._users, starttimes))
 
 
 class SingleMachineAttack:
@@ -142,33 +146,33 @@ class SingleMachineAttack:
             User(user=machine_id) + LoginFail(), fail_count
         ) + UserGroup(User(user=machine_id) + Login(), success_count)
 
-    def __call__(self, start: datetime, duration_seconds: int, **kwargs):
-        return self._behavior(start, duration_seconds, **kwargs)
+    def __call__(self, start: datetime, duration_seconds: int) -> list[LoginAttempt]:
+        return self._behavior(start, duration_seconds)
 
 
 class Simulation:
-    def __init__(self, start: datetime, duration: DurationType, **kwargs):
+    def __init__(self, start: datetime, duration: DurationType):
         self._start = start
-        self._duration_seconds = ensuretimedelta(duration).total_seconds()
-        self._sims: list[Callable] = []
-        self._passed_kwargs = kwargs
-        self._kwargs = {"duration_seconds": self._duration_seconds} | kwargs
+        self._duration_seconds = round(ensuretimedelta(duration).total_seconds())
+        self._sims: list[Callable[[datetime, int], list[LoginAttempt]]] = []
 
-    def __add__(self, other):
-        rv = Simulation(self._start, self._duration_seconds, **self._passed_kwargs)
+    def __add__(self, other: "Simulation") -> "Simulation":
+        rv = Simulation(self._start, self._duration_seconds)
         rv._sims.extend(self._sims)
         rv._sims.append(other)
         return rv
 
-    def __iadd(self, other):
+    def __iadd(self, other: "Simulation") -> "Simulation":
         self._sims.append(other)
         return self
 
-    def __call__(self, **kwargs):
+    def __call__(
+        self, start: datetime | None = None, duration_seconds: int | None = None
+    ) -> list[LoginAttempt]:
         starttimes = generate_times(
             self._start, self._duration_seconds, len(self._sims)
         )
         return flatten(
-            sim(start, **(self._kwargs | kwargs))
+            sim(start, self._duration_seconds)
             for start, sim in zip(starttimes, self._sims)
         )
